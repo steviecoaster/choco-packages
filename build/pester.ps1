12a3058d726a922b931680d898b982355cf96e61
Stephen Valdinger <stephen@chocolatey.io>
2019-09-20 10:04:49 -0400

    [keeper-password-manager] try pester 4.6.0

--- build/pester.ps1
+++ build/pester.ps1
@@ -1,5 +1,5 @@
 If(!(Get-Module Pester)){
-    Install-Module -Name Pester -SkipPublisherCheck -Force -RequiredVersion 4.7.1
+    Install-Module -Name Pester -SkipPublisherCheck -Force -RequiredVersion 4.6.0
 }
 
 Invoke-Pester ./tests/package.tests.ps1 -OutputFile "$($env:Build_ArtifactStagingDirectory)\$($env:Package).xml" -OutputFormat 'NUnitXml'
